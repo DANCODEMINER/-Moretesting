d266f72090fc810e23a4d0c3a07b97b7e8ab853f
DANCODEMINER <adchainminer@gmail.com>
2025-07-13 20:40:38 +0100

    Update miner.js

--- miner.js
+++ miner.js
@@ -751,11 +751,19 @@ function loadMessages() {
     .then(data => {
       const ul = document.getElementById("admin-messages");
       ul.innerHTML = "";
-      data.forEach(msg => {
+
+      if (data.title && data.content) {
         const li = document.createElement("li");
-        li.innerHTML = `<b>${msg.title}</b>: ${msg.content}`;
+        li.innerHTML = `<b>${data.title}</b>: ${data.content}`;
         ul.appendChild(li);
-      });
+      } else {
+        const li = document.createElement("li");
+        li.textContent = "No announcement available.";
+        ul.appendChild(li);
+      }
+    })
+    .catch(err => {
+      console.error("Failed to load message", err);
     });
 }
 
